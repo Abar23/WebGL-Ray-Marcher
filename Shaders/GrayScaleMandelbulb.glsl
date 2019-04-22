33cabad0b5ff845aaa1dd6bb0359e4deca70bf71
Anthony Bartholomew <abar15.abb@gmail.com>
2019-04-22 00:43:32 -0400

    Added screenshots and refactored code

--- Shaders/GrayScaleMandelbulb.glsl
+++ Shaders/GrayScaleMandelbulb.glsl
@@ -90,28 +90,28 @@ float rayMarch(vec3 rayOrigin, vec3 raydrection)
     return 1.0 - (float(steps) / float(MAX_STEPS));
 }
 
-mat3 viewMatrix(vec3 eye, vec3 center, vec3 up) {
+mat3 lookAtViewMatrix(vec3 eye, vec3 center, vec3 up) {
 	vec3 f = normalize(center - eye);
 	vec3 s = normalize(cross(f, up));
 	vec3 u = cross(s, f);
 	return mat3(s, u, -f);
 }
 
-vec3 raydrection(float fieldOfView, vec2 size, vec2 fragCoord) {
-    vec2 xy = fragCoord - size / 2.0;
-    float z = size.y / tan(radians(fieldOfView) / 2.0);
-    return normalize(vec3(xy, -z));
+vec3 rayDirection(float fieldOfView, vec2 size, vec2 fragCoord) {
+    vec2 coordinate = fragCoord - (size / 2.0);
+    float zCoordComponent = -1.0 * (size.y / tan(radians(fieldOfView) / 2.0));
+    return normalize(vec3(coordinate, zCoordComponent));
 }
 
 void main()
 {
     vec3 eyePosition = vec3(4.0, 4.0, 4.0);
     vec3 drectionOfRay = raydrection(45.0, resolution.xy, gl_FragCoord.xy);
-    mat3 rayTransform = viewMatrix(eyePosition, vec3(0.0, 0.0, 0.0), vec3(0.0, 1.0, 0.0));
+    mat3 rayTransform = lookAtViewMatrix(eyePosition, vec3(0.0, 0.0, 0.0), vec3(0.0, 1.0, 0.0));
 
-    vec3 worlddrection = rayTransform * drectionOfRay;
+    vec3 worldDirection = rayTransform * drectionOfRay;
 
-    float grayScale = rayMarch(eyePosition, worlddrection);
+    float grayScale = rayMarch(eyePosition, worldDirection);
 
     gl_FragColor = vec4(grayScale, grayScale, grayScale, 1.0);
 }
